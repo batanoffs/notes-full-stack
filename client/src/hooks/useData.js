59e9441c290272399d8a8266541db13e45285065
batanoffs <batanoff.s@protonmail.com>
2024-10-30 01:32:03 +0200

    add handlers to use the server api endpoints

--- client/src/hooks/useData.js
+++ client/src/hooks/useData.js
@@ -1,8 +1,8 @@
 import { useState, useCallback } from "react";
-import axios from "axios"
+import axios from "axios";
 
 export const useData = (initialState) => {
-  const [data, setData] = useState(initialState);
+  const [notes, setNotes] = useState(initialState);
   const [value, setValue] = useState("");
 
   const changeHandler = (e) => {
@@ -14,27 +14,28 @@ export const useData = (initialState) => {
     e.preventDefault();
     const target = e.currentTarget;
     const form = new FormData(target);
-
-    console.log(form);
-
     const { note } = Object.fromEntries(form); //getting the input value from the form data
-    
+
     try {
-        const response = axios.
+      const response = axios.post("http://localhost:3000/add", { note });
     } catch (error) {
-        
+      console.log(error);
     }
-    console.log(note);
   };
 
-  const fetchData = useCallback(() => {
-    const getData = ""; //todo
-    setData(getData);
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/");
+      setNotes(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 
   return {
     fetchData,
     value,
+    data,
     changeHandler,
     onSubmitHandler,
   };
